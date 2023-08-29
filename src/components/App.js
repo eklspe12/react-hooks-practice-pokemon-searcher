586a8681e7a1c61c22a63ec834bfc5880e65a331
Spencer <eklund.spencer@outlook.com>
2023-08-28 21:45:36 -0500

    data fetched and logged

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PokemonPage from "./PokemonPage";
 
+
 function App() {
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(()  => {
+    fetch("http://localhost:3001/pokemon").then((r) => r.json())
+    .then((pokemonData) => setPokemons(pokemonData))
+  }, []);
+
   return (
     <div className="App">
-      <PokemonPage />
+      <PokemonPage pokemons={pokemons}/>
     </div>
   );
 }
